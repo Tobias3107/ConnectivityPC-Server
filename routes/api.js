b9da551f6eafca48d435163f0fe128b9bb64d1ba
Tobias Karmann <Tobiaskarmann13@hotmail.com>
2020-12-04 09:27:51 +0100

    Change TEMP -> Uses Core Temp if its avaible otherwise use systeminformation

--- routes/api.js
+++ routes/api.js
@@ -4,18 +4,24 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var si = require('systeminformation');
+var os = require('os-utils');
 
 /* Functions */
 
 function getCoreTempData(callback) {
   fs.readFile('C:\\Program Files\\Core Temp\\Plugins\\localdata.json', 'utf8', (err, data) => {
-    data = JSON.parse(data);
-    // Clear up Array - From 255( or Something) to CPU Cores
-    data.uiLoad = data.uiLoad.slice(0, data.uiCoreCnt); 
-    data.uiTjMax = data.uiTjMax.slice(0, data.uiCoreCnt); // Not Tested. On my Setup there is no Data
-    data.fTemp = data.fTemp.slice(0, data.uiCoreCnt); // Not Tested. On my Setup there is no Data
-    callback(data);
-    return;
+    if(typeof err == undefined) {
+      data = JSON.parse(data);
+      // Clear up Array - From 255( or Something) to CPU Cores
+      data.uiLoad = data.uiLoad.slice(0, data.uiCoreCnt); 
+      data.uiTjMax = data.uiTjMax.slice(0, data.uiCoreCnt); // Not Tested. On my Setup there is no Data
+      data.fTemp = data.fTemp.slice(0, data.uiCoreCnt); // Not Tested. On my Setup there is no Data
+      callback(data);
+      return;
+    } else {
+      callback({errno: 500, message: "an Error occured on opening File. Make sure u have Core Temp and the Plugin installed."});
+      return;
+    }
   });
 }
 
@@ -60,10 +66,24 @@ router.get('/coreTempData', (req, res, next) => {
   })
 });
 
-router.get('/temp', (req, res, next) => {
-  getCoreTempData((data) => {
-    res.json(data.fTemp);
+router.get('/temp', async (req, res, next) => {
+  coreData = { errno: 0 };
+  getCoreTempData((data) => {coreData = data;});
+
+  siUsage = 0;
+  await os.cpuUsage((u) => {
+    siUsage = u;
+    return u;
   });
+
+  if(typeof coreData.errno !== undefined) {
+    siTemp = await si.cpuTemperature((data) => {
+      return data;
+    });
+    res.json({"temp": siTemp, "usage": siUsage});
+  } else {
+    res.json(coreData);
+  }
 });
 
 router.get('/coreUsage', (req, res, next) => {
